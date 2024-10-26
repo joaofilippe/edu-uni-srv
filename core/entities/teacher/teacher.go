e474d338df96e0075527b09933fe0f317cdd549a
João Filippe Rossi Rodrigues <joaofilippe@outlook.com>
2024-10-26 18:42:57 -0300

    feat: add Teacher entity and related functions

---
+++ core/entities/teacher/teacher.go
@@ -0,0 +1,23 @@
+package teacher
+
+type Teacher struct {
+	id       int
+	name     string
+	classIds []int
+}
+
+func NewTeacher(id int, name string, classIds []int) *Teacher {
+	return &Teacher{id: id, name: name, classIds: classIds}
+}
+
+func (t *Teacher) ID() int {
+	return t.id
+}
+
+func (t *Teacher) Name() string {
+	return t.name
+}
+
+func (t *Teacher) ClassIds() []int {
+	return t.classIds
+}

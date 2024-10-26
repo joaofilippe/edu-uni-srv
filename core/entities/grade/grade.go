998bc930bd891fa6fcf2059f9d006cb1bde14815
João Filippe Rossi Rodrigues <joaofilippe@outlook.com>
2024-10-26 20:21:39 -0300

    refactor: Add Grade entity and its constructor
    - Add Grade entity to represent a student's grade in a class.
    - Include fields for id, studentId, teacherId, classId, and grade.
    - Implement a constructor function to create a new Grade instance.

---
+++ core/entities/grade/grade.go
@@ -0,0 +1,27 @@
+package grade
+
+import "github.com/shopspring/decimal"
+
+type Grade struct {
+	id        int
+	studentId int
+	teacherId int
+	classId   int
+	grade     decimal.Decimal
+}
+
+func NewGrade(
+	id,
+	studentId,
+	teacherId,
+	classId int,
+	grade decimal.Decimal,
+) *Grade {
+	return &Grade{
+		id,
+		studentId,
+		teacherId,
+		classId,
+		grade,
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/joaofilippe/edu-uni-srv
 
 go 1.23.0
+
+require github.com/shopspring/decimal v1.4.0 // indirect

aa471fb4cb18c420da84295c34a0c983f631850b
João Filippe Rossi Rodrigues <joaofilippe@outlook.com>
2024-10-26 20:18:57 -0300

    refactor: Update user entity to include additional user types
    - Renamed user types from "Aluno" to "Student" and "Responsavel" to "Guardian"
    - Added new user types "Administrator"
    
    Closes #123

--- core/enums/user_type.go
+++ core/enums/user_type.go
@@ -5,23 +5,26 @@ import "fmt"
 type UserType int
 
 const (
-	Aluno UserType = iota
+	Student UserType = iota
 	Professor
-	Responsavel
+	Guardian
+	Administrator
 )
 
 func (u UserType) String() string {
-	return [...]string{"Aluno", "Professor", "Responsavel"}[u]
+	return [...]string{"Student", "Professor", "Guardian", "Administrator"}[u]
 }
 
 func ParseUserType(s string) (UserType, error) {
 	switch s {
-	case "Aluno":
-		return Aluno, nil
+	case "Student":
+		return Student, nil
 	case "Professor":
 		return Professor, nil
-	case "Responsavel":
-		return Responsavel, nil
+	case "Guardian":
+		return Guardian, nil
+	case "Administrator":
+		return Administrator, nil
 	default:
 		return -1, fmt.Errorf("invalid UserType: %s", s)
 	}

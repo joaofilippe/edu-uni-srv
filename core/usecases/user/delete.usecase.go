335320e4732e002e00816ba2eba9f0d57da5ae9e
João Filippe Rossi Rodrigues <joaofilippe@outlook.com>
2024-11-03 12:58:23 -0300

    feat: Implement Delete and Update use cases for User entity with basic validation

---
+++ core/usecases/user/delete.usecase.go
@@ -0,0 +1,24 @@
+package user
+
+import (
+	"github.com/google/uuid"
+	"github.com/joaofilippe/edu-uni-srv/core/repositories"
+)
+
+type DeleteUseCase struct {
+	UserRepo *repositories.IUserRepo
+}
+
+func NewDeleteUseCase(userRepo *repositories.IUserRepo) *DeleteUseCase {
+	return &DeleteUseCase{
+		UserRepo: userRepo,
+	}
+}
+
+func (u *DeleteUseCase) Execute(id uuid.UUID) error {
+	if id == uuid.Nil {
+		return nil
+	}
+
+	return nil
+}
---
+++ core/usecases/user/update.usecase.go
@@ -0,0 +1,24 @@
+package user
+
+import (
+	userEntities "github.com/joaofilippe/edu-uni-srv/core/entities/user"
+	"github.com/joaofilippe/edu-uni-srv/core/repositories"
+)
+
+type UpdateUseCase struct {
+	UserRepo *repositories.IUserRepo
+}
+
+func NewUpdateUseCase(userRepo *repositories.IUserRepo) *UpdateUseCase {
+	return &UpdateUseCase{
+		UserRepo: userRepo,
+	}
+}
+
+func (u *UpdateUseCase) Execute(user *userEntities.User) error {
+	if user == nil {
+		return nil
+	}
+
+	return nil
+}

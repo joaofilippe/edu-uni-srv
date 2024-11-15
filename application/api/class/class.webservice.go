9e92e0ba35db00c65364363bfd33c71d4b52dc0d
João Filippe Rossi Rodrigues <joaofilippe@outlook.com>
2024-11-15 19:47:35 -0300

    feat: implement CreateClass endpoint; handle request binding, error responses, and class creation logic

--- application/api/class/class.webservice.go
+++ application/api/class/class.webservice.go
@@ -1,6 +1,8 @@
 package classweb
 
 import (
+	"github.com/google/uuid"
+	"github.com/joaofilippe/edu-uni-srv/domain/entities/class"
 	iservices "github.com/joaofilippe/edu-uni-srv/domain/services"
 	"github.com/labstack/echo/v4"
 )
@@ -16,5 +18,51 @@ func NewClassWeb(classService *iservices.IClassService) *WebService {
 }
 
 func (cw *WebService) CreateClass(c echo.Context) error {
-	return nil
+	req := new(CreateClassRequest)
+	err := c.Bind(req)
+	if err != nil {
+		return c.JSON(400, struct {
+			Message string `json:"message"`
+			Err     string `json:"error"`
+		}{
+			"Error parsing request",
+			err.Error(),
+		})
+	}
+
+	teacherID, err := uuid.Parse(req.TeacherID)
+	if err != nil {
+		return c.JSON(400, struct {
+			Message string `json:"message"`
+			Err     string `json:"error"`
+		}{
+			"Error parsing teacher ID",
+			err.Error(),
+		})
+	}
+
+	createClass := classentities.NewCreateClass(
+		req.Name,
+		teacherID,
+	)
+
+	classID, err := cw.classService.Create(createClass)
+	if err != nil {
+		return c.JSON(500, struct {
+			Message string `json:"message"`
+			Err     string `json:"error"`
+		}{
+			"Error creating class",
+			err.Error(),
+		})
+	}
+
+	return c.JSON(201, struct {
+		Message string `json:"message"`
+		ClassID string `json:"class_id"`
+	}{
+		"Class created",
+		classID.String(),
+	})
+
 }
